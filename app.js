5737180cc1e88563fb5b97f9a255b3334dd2f7de
Yuri Sementsov <yuri@semencov.com>
2015-02-06 18:53:11 +0200

    in progress

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ tty.on('open', function() {
   console.log("ready");
   //  Give it 10 more seconds to connect to the network, then try to send an SMS 
    
-  tty.modelID(1000).catch(function(err) {
-    console.error(err);
+  tty.serviceProvider().catch(function(err) {
+    console.error("error", err);
   }).done(function(res) {
     console.log("done", res);
   });
